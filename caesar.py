b697b317094ae0fb74a5341dc60b75d9bea85d0b
Sonith Kumar <sonith.kumar@gmail.com>
2024-01-21 09:14:33 +0530

    Initial commit

---
+++ caesar.py
@@ -0,0 +1,8 @@
+def caesar(plaintext, key):
+    ''' Converts the plaintext string passed through the argument with the given key entered by the user.
+    '''
+    
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
